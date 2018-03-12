de32b472585d060706930cdc77ff2f610aed1de7
arqdes_sdesk_spring_mvc_solucao-81618660 <gabriel.ferreria.primo@gmail.com>
2018-03-12 19:46:07 -0300

    81618660

---
+++ src/br/usjt/sdesk/model/service/FilaService.java
@@ -0,0 +1,29 @@
+package br.usjt.sdesk.model.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import br.usjt.sdesk.model.dao.FilaDAO;
+import br.usjt.sdesk.model.entity.Fila;
+
+/**
+ * 
+ * @author Gabriel Primo - 81618660
+ *
+ */
+public class FilaService {
+	private FilaDAO dao;
+	
+	public FilaService(){
+		dao = new FilaDAO();
+	}
+	
+	public ArrayList<Fila> listarFilas() throws IOException{
+		return dao.listarFilas();
+	}
+	
+	public Fila carregar(int id) throws IOException{
+		return dao.carregar(id);
+	}
+
+}
